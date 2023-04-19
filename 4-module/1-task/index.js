f3ce128be9fc7b5676c8fa8bee009dacce88ed10
VictoriaMarsV <108276934+VictoriaMarsV@users.noreply.github.com>
2023-04-19 17:56:47 +0300

    Добавляет задачу 4

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,8 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let ul = document.createElement('ul');
+  friends.forEach(
+    item => ul.innerHTML += `<li>${item.firstName} ${item.lastName}</li>`);
+  return ul;
 }
+
+
