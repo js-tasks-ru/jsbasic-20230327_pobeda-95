a39380886e9eabb4f3979bd271ee293d6091df07
VictoriaMarsV <108276934+VictoriaMarsV@users.noreply.github.com>
2023-04-26 17:47:20 +0300

    В работе задача 2 модуля 2

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -1,3 +1,16 @@
 function isEmpty(obj) {
-  // ваш код...
+  let schedule = {}
+  if (null in schedule) {
+  return false;}
+  else {
+  return true;}
 }
+
+let schedule = {};
+
+alert( isEmpty(schedule) ); // true
+
+schedule["8:30"] = "подъём";
+
+alert( isEmpty(schedule) ); // false
+```
